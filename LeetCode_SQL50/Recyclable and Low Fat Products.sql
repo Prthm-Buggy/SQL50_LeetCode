7b0502b3837f5cf3919c6a80c3c4dcffcf2daf48
Prthm-Buggy <prathamtalele101@gmail.com>
2025-03-17 12:19:20 +0530

    commit

---
+++ LeetCode_SQL50/Recyclable and Low Fat Products.sql
@@ -0,0 +1,52 @@
+-- 1757. Recyclable and Low Fat Products
+-- Table: Products
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_id  | int     |
+-- | low_fats    | enum    |
+-- | recyclable  | enum    |
+-- +-------------+---------+
+
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+-- The result format is in the following example.
+
+select
+    product_id
+from
+    Products
+where  
+    low_fats = 'Y' and recyclable = 'Y';
+
+-- Products table:
+-- +-------------+----------+------------+
+-- | product_id  | low_fats | recyclable |
+-- +-------------+----------+------------+
+-- | 0           | Y        | N          |
+-- | 1           | Y        | Y          |
+-- | 2           | N        | Y          |
+-- | 3           | Y        | Y          |
+-- | 4           | N        | N          |
+-- +-------------+----------+------------+
+-- Output: 
+-- +-------------+
+-- | product_id  |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+-- Explanation: Only products 1 and 3 are both low fat and recyclable.
+
+-- CREATE TABLE Products (
+--     product_id INT PRIMARY KEY,
+--     low_fats ENUM('Y', 'N'),
+--     recyclable ENUM('Y', 'N')
+-- );INSERT INTO Products (product_id, low_fats, recyclable) VALUES
+-- (0, 'Y', 'N'),
+-- (1, 'Y', 'Y'),
+-- (2, 'N', 'Y'),
+-- (3, 'Y', 'Y'),
+-- (4, 'N', 'N');
+
